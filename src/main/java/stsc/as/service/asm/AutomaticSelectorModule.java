738dfe2d99ec32d1f1998b5890e868f3107dc986
Ivan Sidarau <ivan.sidarau@gmail.com>
2016-02-05 00:54:04 +0300

    added initial commit for AutomaticSelectorModule execution class

---
+++ src/main/java/stsc/as/service/asm/AutomaticSelectorModule.java
@@ -0,0 +1,50 @@
+package stsc.as.service.asm;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.TimeZone;
+import java.util.logging.Level;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.XMLConfigurationFactory;
+
+import stsc.common.service.ApplicationHelper;
+import stsc.common.service.StopableApp;
+
+final class AutomaticSelectorModule implements StopableApp {
+
+	static {
+		System.setProperty(XMLConfigurationFactory.CONFIGURATION_FILE_PROPERTY, "./config/log4j2.xml");
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+	private final Logger logger = LogManager.getLogger(AutomaticSelectorModule.class.getName());
+
+	private AutomaticSelectorModule() throws FileNotFoundException, IOException, SQLException {
+	}
+
+	@Override
+	public void start() throws Exception {
+	}
+
+	@Override
+	public void stop() throws Exception {
+	}
+
+	@Override
+	public void log(Level logLevel, String message) {
+		logger.error("log: " + logLevel.getName() + ", message:" + message);
+	}
+
+	public static void main(String[] args) {
+		try {
+			final StopableApp app = new AutomaticSelectorModule();
+			ApplicationHelper.createHelper(app);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
