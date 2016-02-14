d409593c789456984d367837ee5346a43f917e06
Ivan Sidarau <ivan.sidarau@gmail.com>
2016-02-15 00:39:50 +0300

    added example of AutomaticSelectorModule with translation of Experiment (so we could start example of spark searcher and receive some test example)

--- src/main/java/stsc/as/service/asm/AutomaticSelectorModule.java
+++ src/main/java/stsc/as/service/asm/AutomaticSelectorModule.java
@@ -3,6 +3,9 @@ package stsc.as.service.asm;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.TimeZone;
 import java.util.logging.Level;
 
@@ -10,8 +13,20 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.XMLConfigurationFactory;
 
+import stsc.common.algorithms.BadAlgorithmException;
 import stsc.common.service.ApplicationHelper;
 import stsc.common.service.StopableApp;
+import stsc.common.storage.StockStorage;
+import stsc.database.migrations.optimizer.OptimizerDatabaseSettings;
+import stsc.database.service.schemas.optimizer.OrmliteOptimizerExperiment;
+import stsc.database.service.storages.optimizer.OptimizerDatabaseStorage;
+import stsc.distributed.common.types.SimulatorSettingsExternalizable;
+import stsc.distributed.spark.grid.GridSparkStarter;
+import stsc.general.simulator.SimulatorConfiguration;
+import stsc.general.simulator.multistarter.BadParameterException;
+import stsc.general.simulator.multistarter.grid.SimulatorSettingsGridList;
+import stsc.general.strategy.TradingStrategy;
+import stsc.storage.mocks.StockStorageMock;
 
 /**
  * This is as service like (executable) class that automatically read experiment settings from database and start-up experiment on selected distributed system
@@ -28,18 +43,59 @@ final class AutomaticSelectorModule implements StopableApp {
 	private final Logger logger = LogManager.getLogger(AutomaticSelectorModule.class.getName());
 
 	private volatile boolean stopped = false;
-	
+
+	final OptimizerDatabaseStorage optimizerDatabaseStorage;
+
 	private volatile int sleepMicroseconds = 1000;
 
-	private AutomaticSelectorModule() throws FileNotFoundException, IOException, SQLException {
+	private final StockStorage stockStorage = StockStorageMock.getStockStorage();
+
+	private AutomaticSelectorModule(String filepath) throws FileNotFoundException, IOException, SQLException {
+		final OptimizerDatabaseSettings databaseSettings = (filepath != null) ? //
+				new OptimizerDatabaseSettings(filepath)
+				: //
+				new OptimizerDatabaseSettings("./config/optimizer_production.properties");
+		this.optimizerDatabaseStorage = new OptimizerDatabaseStorage(databaseSettings);
 	}
 
 	@Override
 	public void start() throws Exception {
 		while (!stopped) {
-			
+			final Optional<OrmliteOptimizerExperiment> experiment = optimizerDatabaseStorage.getExperiments().bookExperiment();
+			if (experiment.isPresent()) {
+				solveExperiment(experiment.get());
+			}
 			Thread.sleep(sleepMicroseconds);
 		}
+	}
+
+	private void solveExperiment(OrmliteOptimizerExperiment experiment) {
+		try {
+			executeSolver(experiment);
+			experiment.setProcessed();
+			optimizerDatabaseStorage.getExperiments().saveExperiment(experiment);
+		} catch (Exception e) {
+			try {
+				optimizerDatabaseStorage.getExperiments().deleteLock(experiment);
+			} catch (SQLException de) {
+				logger.error("while deleting lock for ", experiment, de);
+			}
+		}
+	}
+
+	private void executeSolver(OrmliteOptimizerExperiment ormliteOptimizerExperiment) throws SQLException, BadParameterException, BadAlgorithmException {
+		final ExperimentTransformer experimentTransformer = new ExperimentTransformer(optimizerDatabaseStorage.getExperiments());
+		final SimulatorSettingsGridList experiment = experimentTransformer.transform(stockStorage, ormliteOptimizerExperiment);
+
+		final ArrayList<SimulatorSettingsExternalizable> externalizableExperiment = new ArrayList<>();
+		for (SimulatorConfiguration ss : experiment) {
+			externalizableExperiment.add(new SimulatorSettingsExternalizable(ss));
+		}
+
+		final GridSparkStarter gridSparkStarter = new GridSparkStarter();
+		final List<TradingStrategy> tradingStrategies = gridSparkStarter.searchOnSpark(externalizableExperiment); 
+		// TODO load settings for search from database
+		// TODO write them to database;
 	}
 
 	@Override
@@ -54,7 +110,8 @@ final class AutomaticSelectorModule implements StopableApp {
 
 	public static void main(String[] args) {
 		try {
-			final StopableApp app = new AutomaticSelectorModule();
+			final String filepath = (args.length > 0) ? args[0] : null;
+			final StopableApp app = new AutomaticSelectorModule(filepath);
 			ApplicationHelper.createHelper(app, (e) -> {
 				e.printStackTrace();
 				return true;
---
+++ src/main/java/stsc/as/service/asm/ExperimentTransformer.java
@@ -0,0 +1,84 @@
+package stsc.as.service.asm;
+
+import java.security.InvalidParameterException;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+import stsc.common.FromToPeriod;
+import stsc.common.algorithms.AlgorithmType;
+import stsc.common.storage.StockStorage;
+import stsc.database.service.schemas.optimizer.OrmliteOptimizerDoubleParameter;
+import stsc.database.service.schemas.optimizer.OrmliteOptimizerExecution;
+import stsc.database.service.schemas.optimizer.OrmliteOptimizerExperiment;
+import stsc.database.service.schemas.optimizer.OrmliteOptimizerIntegerParameter;
+import stsc.database.service.schemas.optimizer.OrmliteOptimizerStringParameter;
+import stsc.database.service.schemas.optimizer.OrmliteOptimizerSubExecutionParameter;
+import stsc.database.service.storages.optimizer.OptimizerExperimentsDatabaseStorage;
+import stsc.general.simulator.multistarter.AlgorithmSettingsIteratorFactory;
+import stsc.general.simulator.multistarter.BadParameterException;
+import stsc.general.simulator.multistarter.MpDouble;
+import stsc.general.simulator.multistarter.MpInteger;
+import stsc.general.simulator.multistarter.MpString;
+import stsc.general.simulator.multistarter.MpSubExecution;
+import stsc.general.simulator.multistarter.grid.SimulatorSettingsGridFactory;
+import stsc.general.simulator.multistarter.grid.SimulatorSettingsGridList;
+
+final class ExperimentTransformer {
+
+	final private OptimizerExperimentsDatabaseStorage optimizerExperimentsDatabaseStorage;
+
+	ExperimentTransformer(final OptimizerExperimentsDatabaseStorage optimizerExperimentsDatabaseStorage) {
+		this.optimizerExperimentsDatabaseStorage = optimizerExperimentsDatabaseStorage;
+	}
+
+	// translator
+
+	SimulatorSettingsGridList transform(final StockStorage stockStorage, OrmliteOptimizerExperiment ormliteExperiment) throws SQLException, BadParameterException {
+		final FromToPeriod period = new FromToPeriod(ormliteExperiment.getPeriodFrom(), ormliteExperiment.getPeriodTo());
+		final SimulatorSettingsGridFactory factory = new SimulatorSettingsGridFactory(stockStorage, period);
+
+		final List<OrmliteOptimizerExecution> loadExecutions = optimizerExperimentsDatabaseStorage.loadExecutions(ormliteExperiment);
+
+		for (OrmliteOptimizerExecution execution : loadExecutions) {
+			if (execution.getAlgorithmType().equals(AlgorithmType.STOCK_VALUE.getValue())) {
+				final AlgorithmSettingsIteratorFactory algorithmFactory = transformExecution(execution);				
+				factory.addStock(execution.getExecutionName(), execution.getAlgorithmName(), algorithmFactory.getGridIterator());				
+			} else if (execution.getAlgorithmType().equals(AlgorithmType.EOD_VALUE.getValue())) {
+				final AlgorithmSettingsIteratorFactory algorithmFactory = transformExecution(execution);				
+				factory.addEod(execution.getExecutionName(), execution.getAlgorithmName(), algorithmFactory.getGridIterator());				
+			} else {
+				throw new InvalidParameterException("Wrong algorithm type (" + execution.getAlgorithmType() + ") for execution: " + execution.getId() + " : "
+						+ execution.getExecutionName());
+			}
+		}
+
+		return null;
+	}
+
+	private AlgorithmSettingsIteratorFactory transformExecution(OrmliteOptimizerExecution execution) throws SQLException, BadParameterException {
+		AlgorithmSettingsIteratorFactory algorithmFactory = new AlgorithmSettingsIteratorFactory();
+		
+		final List<OrmliteOptimizerDoubleParameter> loadDoubleParameters = optimizerExperimentsDatabaseStorage.loadDoubleParameters(execution);
+		for(OrmliteOptimizerDoubleParameter dp : loadDoubleParameters) {
+			algorithmFactory.add(new MpDouble(dp.getParameterName(), dp.getFrom(), dp.getTo(), dp.getStep()));
+		}
+		final List<OrmliteOptimizerIntegerParameter> loadIntegerParameters = optimizerExperimentsDatabaseStorage.loadIntegerParameters(execution);
+		for(OrmliteOptimizerIntegerParameter dp : loadIntegerParameters) {
+			algorithmFactory.add(new MpInteger(dp.getParameterName(), dp.getFrom(), dp.getTo(), dp.getStep()));
+		}
+		final List<OrmliteOptimizerStringParameter> loadStringParameters = optimizerExperimentsDatabaseStorage.loadStringParameters(execution);
+		for(OrmliteOptimizerStringParameter dp : loadStringParameters) {
+			algorithmFactory.add(new MpString(dp.getParameterName(), Lists.newArrayList(dp.getParameterDomen().split("|"))));
+		}
+		final List<OrmliteOptimizerSubExecutionParameter> loadSubExecutionParameters = optimizerExperimentsDatabaseStorage.loadSubExecutionParameters(execution);
+		for(OrmliteOptimizerSubExecutionParameter dp : loadSubExecutionParameters) {
+			algorithmFactory.add(new MpSubExecution(dp.getParameterName(), Lists.newArrayList(dp.getParameterDomen().split("|"))));
+		}
+		return algorithmFactory;
+	}
+
+	
+	
+}
